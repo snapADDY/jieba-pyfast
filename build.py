dceb6077151b6b2ba1c22d8a40f1fe97f90b14bf
adam444555 <a473489548@gmail.com>
2022-04-11 14:40:31 +0200

    typo

--- build.py
+++ build.py
@@ -3,6 +3,6 @@ from setuptools import Extension
 def build(setup_kwargs: dict):
     setup_kwargs["ext_modules"] = [
         Extension('_jieba_fast_functions_py3',
-        sources=['jieba_fast/source/jieba_fast_functions_wrap_py3_wrap.c'],
+        sources=['jieba_pyfast/source/jieba_fast_functions_wrap_py3_wrap.c'],
         )
     ]
